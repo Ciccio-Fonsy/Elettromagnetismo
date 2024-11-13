0846f5e6e76f38724020b77d9aa072d3669f6dba
milacasali <76772629+milacasali@users.noreply.github.com>
2024-11-13 18:05:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,24 +32,32 @@ int main(){
     
     
     TH1D* h_mass_invariant = new TH1D("h_mass_invariant", "Mass Invariant distribution", 100, 0, 15);
+    TH1D* hMassOppositeSign = new TH1D("hMassOppositeSign", "Massa Invariante - Carica Discorre", 100, 0, 2);
+    TH1D* hMassSameSign = new TH1D("hMassSameSign", "Massa Invariante - Carica Concorde", 100, 0, 2);
+    TH1D* hMassPionKaonOpposite = new TH1D("hMassPionKaonOpposite", "Massa Invariante - Pione+/Kaone- e Pione-/Kaone+", 100, 0, 2);
+    TH1D* hMassPionKaonSame = new TH1D("hMassPionKaonSame", "Massa Invariante - Pione+/Kaone+ e Pione-/Kaone-", 100, 0, 2);
+    TH1D* hMassKStarDecay = new TH1D("hMassKStarDecay", "Massa Invariante - Decadimento Risonanza K*", 100, 0.7, 1.0);
     TH1D* h_type = new TH1D("h_type", "Particle type distribution", 7, 0, 7);
-    TH1D* h_charge = new TH1D("h_charge", "Charge distribution", 3, -1, 2);
-    TH1D* h_width = new TH1D("h_width", "Width distribution", 100, 0, 0.1);
     TH1D* h_energy = new TH1D("energy", "Energy distribution", 100, 0, 2);
     TH1D* h_theta = new TH1D("theta", "Theta distribution", 100, -M_PI/2, M_PI/2);
     TH1D* h_phi = new TH1D("phi", "Phi distribution", 100, 0, 2*M_PI);
     TH1D* h_pout = new TH1D("pout", "Pout distribution", 100, 0, 1);
     TH1D* h_Ptrasv = new TH1D("Ptrasve", "Ptrasv distribution", 100, 0, 1);
 
-
-
+    double total_energy = 0;
+    double total_px = 0;
+    double total_py = 0;
+    double total_pz = 0;
 
     for (int i=0; i <= n_event; ++i)
     {
            int particle_count=0;
+           double mass_inavraint = 0;
+        
         for(int j=0; j <= n_particles_event; ++j)
         {
             Particle new_particle = createRandomParticle();
+           
             if (new_particle.get_name() == '*')
             {
                 char dau1_name = 0;
@@ -67,23 +75,43 @@ int main(){
                 break;}
                 
                 event_particles[particle_count] = Particle(dau1_name);
-                event_particles[particle_count + 1] = Particle(dau2_name);
-                new_particle.decay2body(event_particles[particle_count], event_particles[particle_count + 1]);
+                
+                event_particles[particle_count + 1] = Particle(dau2_name);                
+                
+                new_particle.decay2body(event_particles[particle_count + 1], event_particles[particle_count + 1]);
+               
+                total_energy += (event_particles[particle_count].get_energy() + event_particles[particle_count + 1].get_energy());
+                total_px += (event_particles[particle_count].get_px() + event_particles[particle_count + 1].get_px());
+                total_py += (event_particles[particle_count].get_py() + event_particles[particle_count + 1].get_py());
+                total_pz += (event_particles[particle_count].get_pz() + event_particles[particle_count + 1].get_pz());
+                
+                hMassKStarDecay->Fill(event_particles[particle_count].invMass(event_particles[particle_count + 1]));
+
                 particle_count += 2;
             } else {
+
                 event_particles[particle_count] = new_particle;
+
+                total_energy += new_particle.get_energy();
+                total_px += new_particle.get_px();  
+                total_py += new_particle.get_py();
+                total_pz += new_particle.get_pz();
+
                 ++particle_count;
             }
         }
-        double mass_invariant = 0;
-        for (int j=0; j <= particle_count; ++j)
-        {
-            mass_invariant += event_particles[j].get_mass();
-        }
+
+        mass_inavraint = std::sqrt(total_energy*total_energy - total_px*total_px - total_py*total_py - total_pz*total_pz);
+        h_mass_invariant->Fill(mass_inavraint);
+        //riempimento istogrammi
+
+
+
     }
 
-    return 0;
+    double mass_invariant = std::sqrt(total_energy*total_energy - total_px*total_px - total_py*total_py - total_pz*total_pz);
 
+    return 0;
 }
 
 void randomParticlePosition(Particle& particle)
@@ -123,4 +151,47 @@ Particle createRandomParticle()
     Particle particle(name);
     randomParticlePosition(particle);
     return particle;
+}
+
+void fillInstogram(Particle particle[120], TH1D* hMassOppositeSign, TH1D* hMassSameSign, TH1D* hMassPionKaonOpposite, TH1D* hMassPionKaonSame, TH1D* hMassKStarDecay, TH1D* h_type, TH1D* h_energy, TH1D* h_theta, TH1D* h_phi, TH1D* h_pout, TH1D* h_Ptrasv)
+{
+    for (int i = 0; i < n_particles; ++i) {
+        //altri istogtammi credo
+        h_type->Fill(particle[i].get_name());
+        h_energy->Fill(particle[i].get_energy());
+        h_theta->Fill(std::acos(particle[i].get_pz()/std::sqrt(particle[i].get_px()*particle[i].get_px() + particle[i].get_py()*particle[i].get_py() + particle[i].get_pz()*particle[i].get_pz())));
+        h_phi->Fill(std::atan(particle[i].get_py()/particle[i].get_px()));
+        h_pout->Fill(std::sqrt(particle[i].get_px()*particle[i].get_px() + particle[i].get_py()*particle[i].get_py() + particle[i].get_pz()*particle[i].get_pz()));
+        h_Ptrasv->Fill(std::sqrt(particle[i].get_px()*particle[i].get_px() + particle[i].get_py()*particle[i].get_py()));
+    
+            for (int j = i + 1; j < n_particles; ++j) {
+                // Calcola la massa invariante tra particella i e particella j(assoulutamente inutile)
+                double px_sum = particles[i].get_px() + particles[j].get_px();
+                double py_sum = particles[i].get_py() + particles[j].get_py();
+                double pz_sum = particles[i].get_pz() + particles[j].get_pz();
+                double E_sum = particles[i].get_energy() + particles[j].get_energy();
+                double mass_inv = std::sqrt(E_sum * E_sum - px_sum * px_sum - py_sum * py_sum - pz_sum * pz_sum);
+
+//calcio la massa invariante tra le particelle i e j di base e la metto negli istogrammi solo se le part soddisfano le cond
+                // Istogrammi basati su carica discordante (cariche opposte)
+                if (particles[i].get_charge() * particles[j].get_charge() < 0) {
+                    hDiscordantCharge->Fill(mass_inv);  
+                }
+
+                // Istogrammi basati su carica concordante (cariche uguali)
+                else if (particles[i].charge * particles[j].charge > 0) {
+                    hConcordantCharge->Fill(mass_inv);;
+                }
+//non modificasti fatti cchet  
+                // Massa invariante tra particelle di tipo Pion+/Kaon- e Pion-/Kaon+
+                if ((particles[i].type == 0 && particles[j].type == 3) || (particles[i].type == 1 && particles[j].type == 2)) {
+                    hPionKaon1->Fill(mass_inv);
+                }
+
+                // Massa invariante tra particelle di tipo Pion+/Kaon+ e Pion-/Kaon-
+                if ((particles[i].type == 0 && particles[j].type == 2) || (particles[i].type == 1 && particles[j].type == 3)) {
+                    hPionKaon2->Fill(mass_inv);
+                }
+            }
+        }
 }
