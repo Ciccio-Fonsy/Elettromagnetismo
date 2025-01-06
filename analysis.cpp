444b2afda0d4e5a44f162538e2b1e83ad78c76c5
milacasali <76772629+milacasali@users.noreply.github.com>
2025-01-06 16:09:00 +0100

    Update analysis.cpp

--- analysis.cpp
+++ analysis.cpp
@@ -13,10 +13,22 @@ void analyze_histograms() {
         return;
     }
 
+  
     // Caricamento degli istogrammi
+    TH1F *hMassInvariant = (TH1F*)file->Get("hMassInvariant");
+    TH1F *hMassOppositeSign = (TH1F*)file->Get("hMassOppositeSign");
+    TH1F *hMassSameSign = (TH1F*)file->Get("hMassSameSign");
+    TH1F *hMassPionKaonOpposite = (TH1F*)file->Get("hMassPionKaonOpposite");
+    TH1F *hMassPionKaonSame = (TH1F*)file->Get("hMassPionKaonSame");
+    TH1F *hMassKStarDecay = (TH1F*)file->Get("hMassKStarDecay");
+    TH1F *h_type = (TH1F*)file->Get("h_type");
+    TH1F *h_energy = (TH1F*)file->Get("h_energy");
+    TH1F *h_theta = (TH1F*)file->Get("h_theta");
     TH1F *h_phi = (TH1F*)file->Get("h_phi");
     TH1F *h_pout = (TH1F*)file->Get("h_pout");
-    TH1F *hMassInvariant = (TH1F*)file->Get("hMassInvariant");
+    TH1F *h_Ptrasv = (TH1F*)file->Get("h_Ptrasv");
+
+
 
     if (!h_phi || !h_pout || !hMassInvariant) {
         std::cerr << "Errore nel caricamento degli istogrammi." << std::endl;
@@ -29,17 +41,10 @@ void analyze_histograms() {
     std::cout << "Numero di ingressi in h_pout: " << h_pout->GetEntries() << std::endl;
     std::cout << "Numero di ingressi in h_mass_invariant: " << hMassInvariant->GetEntries() << std::endl;
 
-// Fit della distribuzione angolare con una funzione uniforme
-h_phi->Fit("pol0");  // "pol0" è una funzione predefinita per un fit costante
-TF1* f_uniform = h_phi->GetFunction("pol0");
-
-if (f_uniform) {
+    // Fit della distribuzione angolare con una funzione uniforme
+    TF1 *f_uniform = new TF1("f_uniform", "[0]", 0, 2 * M_PI);
+    h_phi->Fit("f_uniform");
     f_uniform->Print();
-    std::cout << "Chi2/NDF per il fit uniforme: " << f_uniform->GetChisquare() / f_uniform->GetNDF() << std::endl;
-    std::cout << "Probabilità del fit uniforme: " << f_uniform->GetProb() << std::endl;
-} else {
-    std::cerr << "Errore nel recupero della funzione di fit." << std::endl;
-}
 
     // Fit della distribuzione del modulo dell'impulso con una funzione esponenziale
     TF1 *f_exponential = new TF1("f_exponential", "[0]*exp(-[1]*x)", 0, 10);
