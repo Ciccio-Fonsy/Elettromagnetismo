b8c625bd52913b88c0060c66f0b1d72c104dd889
Ciccio-Fonsy <fonsecafrancesco04@gmail.com>
2025-01-09 19:12:18 +0100

    Add files via upload

--- analysis.cpp
+++ analysis.cpp
@@ -70,9 +70,9 @@ int main() {
 
   // Numero di ingressi
   for (int i = 0; i < 12; ++i) {
-    std::cout << "Entries in " << h_array[i]->GetName() << ": "
-              << h_array[i]->GetEntries() << ", expected: " << expected_array[i]
-              << std::endl;
+    std::cout << "Entries in " << std::setw(21) << h_array[i]->GetName() << ": "
+              << std::setw(11) << h_array[i]->GetEntries()
+              << ", expected: " << expected_array[i] << std::endl;
   }
   std::cout << '\n';
 
@@ -87,24 +87,28 @@ int main() {
   std::cout << '\n';
 
   // Fit della distribuzione angolare con una funzione uniforme
-  TF1* f_uniform = new TF1("f_uniform", "[0]");
+  TF1* f_uniform = new TF1("f_uniform", "[0] * 100000");
   f_uniform->Print();
   std::cout << '\n';
 
   std::cout << "\u03C6 uniform fit results:" << std::endl;
   hPhi->Fit("f_uniform", "Q");
-  std::cout << "  Constant:\t" << f_uniform->GetParameter(0) << " +/- "
+  std::cout << "  Constant:\t" << f_uniform->GetParameter(0) << " ± "
             << f_uniform->GetParError(0) << std::endl;
-  std::cout << "  Chi2/NDF:\t"
+  std::cout << "  \u03C7²:\t\t" << f_uniform->GetChisquare() << std::endl;
+  std::cout << "  NDF:\t\t" << f_uniform->GetNDF() << std::endl;
+  std::cout << "  \u03C7²/NDF:\t"
             << f_uniform->GetChisquare() / f_uniform->GetNDF() << std::endl;
   std::cout << "  Probability:\t" << f_uniform->GetProb() << std::endl;
   std::cout << '\n';
 
   std::cout << "\u03B8 uniform fit results:" << std::endl;
   hTheta->Fit("f_uniform", "Q");
-  std::cout << "  Constant:\t" << f_uniform->GetParameter(0) << " +/- "
+  std::cout << "  Constant:\t" << f_uniform->GetParameter(0) << " ± "
             << f_uniform->GetParError(0) << std::endl;
-  std::cout << "  Chi2/NDF:\t"
+  std::cout << "  \u03C7²:\t\t" << f_uniform->GetChisquare() << std::endl;
+  std::cout << "  NDF:\t\t" << f_uniform->GetNDF() << std::endl;
+  std::cout << "  \u03C7²/NDF:\t"
             << f_uniform->GetChisquare() / f_uniform->GetNDF() << std::endl;
   std::cout << "  Probability:\t" << f_uniform->GetProb() << std::endl;
   std::cout << '\n';
@@ -116,15 +120,20 @@ int main() {
   f_exponential->Print();
   std::cout << "Momentum exponential fit results:" << std::endl;
   hPout->Fit("f_exponential", "Q");
-  std::cout << "  Intercept:\t" << f_exponential->GetParameter(0) << " +/- "
+  std::cout << "  Intercept:\t" << f_exponential->GetParameter(0) << " ± "
             << f_exponential->GetParError(0) << std::endl;
-  std::cout << "  Slope:\t" << f_exponential->GetParameter(1) << " +/- "
+  std::cout << "  Slope:\t" << f_exponential->GetParameter(1) << " ± "
             << f_exponential->GetParError(1) << std::endl;
-  std::cout << "  Chi2/NDF:\t"
+  std::cout << "  Average:\t" << f_exponential->Mean(0, 6) << " ± "
+            << f_exponential->GetParError(1) * f_exponential->Mean(0, 6)
+                   * f_exponential->Mean(0, 6)
+            << std::endl;
+  std::cout << "  \u03C7²:\t\t" << f_exponential->GetChisquare() << std::endl;
+  std::cout << "  NDF:\t\t" << f_exponential->GetNDF() << std::endl;
+  std::cout << "  \u03C7²/NDF:\t"
             << f_exponential->GetChisquare() / f_exponential->GetNDF()
             << std::endl;
   std::cout << "  Probability:\t" << f_exponential->GetProb() << std::endl;
-  std::cout << "Average momentum: " << hPout->GetMean() << std::endl;
   std::cout << '\n';
 
   // sottraggo gli istogrammi delle masse invarianti con carica opposta e stessa
@@ -169,18 +178,36 @@ int main() {
   TF1* f_gaussian = new TF1("f_gaussian", "gaus(0)", 0.7, 1.1);
 
   // Eseguiamo il fit sull'istogramma risultato dalla sottrazione
+  hMassKStarDecay->Fit("f_gaussian", "Q", nullptr, 0.7, 1.1);
+
+  // Stampiamo i parametri del fit
+  std::cout << "True K* decays gaussian fit results:" << std::endl;
+  std::cout << "  K* mass:\t" << f_gaussian->GetParameter(1) << " ± "
+            << f_gaussian->GetParError(1) << std::endl;
+  std::cout << "  K* width:\t" << f_gaussian->GetParameter(2) << " ± "
+            << f_gaussian->GetParError(2) << std::endl;
+  std::cout << "  \u03C7²:\t\t" << f_gaussian->GetChisquare() << std::endl;
+  std::cout << "  NDF:\t\t" << f_gaussian->GetNDF() << std::endl;
+  std::cout << "  \u03C7²/NDF:\t"
+            << f_gaussian->GetChisquare() / f_gaussian->GetNDF() << std::endl;
+  std::cout << "  Probability:\t" << f_gaussian->GetProb() << std::endl;
+  std::cout << '\n';
+
+  // Eseguiamo il fit sull'istogramma risultato dalla sottrazione
   hInvariantMassSub1->Fit("f_gaussian", "Q", nullptr, 0.7, 1.1);
 
   // Stampiamo i parametri del fit
   std::cout << "Concordant - discordant particles gaussian fit results:"
             << std::endl;
-  std::cout << "K* mass: " << f_gaussian->GetParameter(1) << " +/- "
+  std::cout << "  K* mass:\t" << f_gaussian->GetParameter(1) << " ± "
             << f_gaussian->GetParError(1) << std::endl;
-  std::cout << "K* width: " << f_gaussian->GetParameter(2) << " +/- "
+  std::cout << "  K* width:\t" << f_gaussian->GetParameter(2) << " ± "
             << f_gaussian->GetParError(2) << std::endl;
-  std::cout << "Chi2/NDF: " << f_gaussian->GetChisquare() / f_gaussian->GetNDF()
-            << std::endl;
-  std::cout << "Probability: " << f_gaussian->GetProb() << std::endl;
+  std::cout << "  \u03C7²:\t\t" << f_gaussian->GetChisquare() << std::endl;
+  std::cout << "  NDF:\t\t" << f_gaussian->GetNDF() << std::endl;
+  std::cout << "  \u03C7²/NDF:\t"
+            << f_gaussian->GetChisquare() / f_gaussian->GetNDF() << std::endl;
+  std::cout << "  Probability:\t" << f_gaussian->GetProb() << std::endl;
   std::cout << '\n';
 
   // Eseguiamo il fit sull'istogramma risultato dalla sottrazione
@@ -189,13 +216,15 @@ int main() {
   // Stampiamo i parametri del fit
   std::cout << "Concordant - discordant \u03C0/K couples gaussian fit results:"
             << std::endl;
-  std::cout << "K* mass: " << f_gaussian->GetParameter(1) << " +/- "
+  std::cout << "  K* mass:\t" << f_gaussian->GetParameter(1) << " ± "
             << f_gaussian->GetParError(1) << std::endl;
-  std::cout << "K* width: " << f_gaussian->GetParameter(2) << " +/- "
+  std::cout << "  K* width:\t" << f_gaussian->GetParameter(2) << " ± "
             << f_gaussian->GetParError(2) << std::endl;
-  std::cout << "Chi2/NDF: " << f_gaussian->GetChisquare() / f_gaussian->GetNDF()
-            << std::endl;
-  std::cout << "Probability: " << f_gaussian->GetProb() << std::endl;
+  std::cout << "  \u03C7²:\t\t" << f_gaussian->GetChisquare() << std::endl;
+  std::cout << "  NDF:\t\t" << f_gaussian->GetNDF() << std::endl;
+  std::cout << "  \u03C7²/NDF:\t"
+            << f_gaussian->GetChisquare() / f_gaussian->GetNDF() << std::endl;
+  std::cout << "  Probability:\t" << f_gaussian->GetProb() << std::endl;
   std::cout << '\n';
 
   while (gROOT->GetListOfCanvases()->FindObject("c1")) {
